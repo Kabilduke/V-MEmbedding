7a751b1784f1d0f908eb92385d8aa4760ab49853
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2025-02-23 20:17:01 +0530

    Create Architecture_1DCNN:1.py

---
+++ Architecture_1DCNN:1.py
@@ -0,0 +1,29 @@
+Emodel = models.Sequential([
+    layers.Embedding(input_dim = vocal_size, output_dim = 256), #With 256-dimensions
+    
+    layers.Conv1D(64, kernel_size = 3, activation = 'relu'),
+    layers.MaxPooling1D(pool_size = 4),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    
+    layers.Conv1D(32, kernel_size = 3, activation = 'elu'),
+    layers.MaxPooling1D(pool_size = 3),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    
+    layers.Flatten(),
+    layers.Dense(16, activation = 'elu'),
+    layers.Dropout(0.2),
+    
+    layers.Dense(6, activation = 'softmax')
+])
+
+Emodel.build(input_shape =(None, max_length))
+
+Emodel.compile(
+    optimizer='rmsprop', 
+    loss='sparse_categorical_crossentropy', 
+    metrics=['accuracy']
+)
+
+Emodel.summary()
