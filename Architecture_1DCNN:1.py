bdd5ecd0fe51a60043184662aa5051bbe8f22fdd
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2025-02-23 20:20:47 +0530

    Update Architecture_1DCNN:1.py

--- Architecture_1DCNN:1.py
+++ Architecture_1DCNN:1.py
@@ -1,5 +1,5 @@
 Emodel = models.Sequential([
-    layers.Embedding(input_dim = vocal_size, output_dim = 256), #With 256-dimensions
+    layers.Embedding(input_dim = vocal_size, output_dim = 256), #With Vocal Size = 10000, 256-dimensions
     
     layers.Conv1D(64, kernel_size = 3, activation = 'relu'),
     layers.MaxPooling1D(pool_size = 4),
@@ -18,7 +18,7 @@ Emodel = models.Sequential([
     layers.Dense(6, activation = 'softmax')
 ])
 
-Emodel.build(input_shape =(None, max_length))
+Emodel.build(input_shape =(None, max_length)) #Max_length = 66
 
 Emodel.compile(
     optimizer='rmsprop', 
